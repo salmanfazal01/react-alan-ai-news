a2f7db9441d92dd8f288a771b82722163a31ca81
Salman Fazal <rustamas@uni.coventry.ac.uk>
2020-08-16 16:25:22 -0400

    Alan AI app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {Avatar, Typography} from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./AppStyles";
+
+const alanKey =
+    "fbf5186cc70b744e1a83ca2c12e5e0982e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+    const classes = useStyles();
+
+    const [activeArticle, setActiveArticle] = useState(0);
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles, number }) => {
+                if (command === "newHeadlines") {
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if (command === "highlight") {
+                    setActiveArticle(
+                        (prevActiveArticle) => prevActiveArticle + 1
+                    );
+                } else if (command === "open") {
+                    const parsedNumber =
+                        number.length > 2
+                            ? wordsToNumbers(number, { fuzzy: true })
+                            : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if (parsedNumber > 20) {
+                        alanBtn().playText("Please try that again...");
+                    } else if (article) {
+                        window.open(article.url, "_blank");
+                        alanBtn().playText("Opening...");
+                    } else {
+                        alanBtn().playText("Please try that again...");
+                    }
+                }
+            },
+        });
+    }, []);
+
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                {newsArticles.length ? (
+                    <div className={classes.infoContainer}>
+                        <div className={classes.card}>
+                            <Typography variant="h5" component="h2">
+                                Try saying: <br />
+                                <br />
+                                Open article number [4]
+                            </Typography>
+                        </div>
+                        <div className={classes.card}>
+                            <Typography variant="h5" component="h2">
+                                Try saying: <br />
+                                <br />
+                                Go back
+                            </Typography>
+                        </div>
+                    </div>
+                ) : null}
+                <img
+                    src="https://alan.app/voice/images/previews/preview.jpg"
+                    className={classes.alanLogo}
+                    alt="logo"
+                />
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+            {!newsArticles.length ? (
+                <div className={classes.footer}>
+                    <Typography variant="body1" component="h2">
+                        Created by
+                        <a
+                            className={classes.link}
+                            href="https://github.com/salmanfazal01">
+                            {" "}
+                            Salman Fazal
+                        </a>{" "}
+                        -
+                        <a
+                            className={classes.link}
+                            href="https://www.youtube.com/channel/UCSEy8lBnjp42x5W1JPcca0w">
+                            {" "}
+                            Youtube
+                        </a>
+                    </Typography>
+                    <Avatar
+                        className={classes.image}
+                        src={'https://avatars1.githubusercontent.com/u/15085416?s=460&u=82e1033276c0cdb01e27ee4b1e3f09c0b5162a75&v=4'}
+                        height="50px"
+                        alt="Salman Fazal"
+                    />
+                </div>
+            ) : null}
+        </div>
+    );
+};
 
 export default App;
